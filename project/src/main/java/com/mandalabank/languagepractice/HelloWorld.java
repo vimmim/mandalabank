4aac6eb10c32771390e9ab67bfee0c6f47f37149
Vimala Mandala <mandala.vimala@gmail.com>
2020-12-25 21:59:49 -0500

    first checkin

---
+++ project/src/main/java/com/mandalabank/languagepractice/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.mandalabank.languagepractice;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+        int a = 20;
+        a = a + 20;
+        a = a * 3;
+        System.out.println("output is: " + a);
+    }
+}
---
+++ project/src/main/java/com/mandalabank/languagepractice/IfConditionPractice.java
@@ -0,0 +1,16 @@
+package com.mandalabank.languagepractice;
+
+public class IfConditionPractice {
+    public static void main(String[] args) {
+        // comments
+        /*
+            multi line comments
+         */
+        int a; // 8 byte round numbers - 2pow32 to +2pow32 numbers
+        float b; // 8 byte decimal numbers
+        double c; // 8 byte decimal numbers
+        char d; //
+        boolean e;
+
+    }
+}
